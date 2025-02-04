5aa0b9581a4924006096bc09597ec699e1c2a254
Ivan Yurchenko <ivan@ivanyu.me>
2025-02-04 20:48:41 +0200

    Add Gradle skeleton

---
+++ app/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    application
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(libs.junit.jupiter)
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+application {
+    mainClass = "me.ivanyu.icebreaker.App"
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+}
---
+++ app/src/main/java/me/ivanyu/icebreaker/App.java
@@ -0,0 +1,6 @@
+package me.ivanyu.icebreaker;
+
+public class App {
+    public static void main(String[] args) {
+    }
+}
